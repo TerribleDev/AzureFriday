8a39c8e8dc1c2660f95ae68720c9298a0cf5775d
Darren Cauthon <darren@cauthon.com>
2014-05-28 07:25:02 -0500

    Use spaces.

--- getazure.rb
+++ getazure.rb
@@ -9,9 +9,9 @@ xml = Nokogiri::XML(open('http://s.ch9.ms/Shows/Azure-Friday/feed/mp4high'))
 item = xml.xpath('//item')
 
 item.each do |s|
-next if File.exist? s.at('enclosure')['url'].to_s.split('/').last
-File.open("#{s.at('enclosure')['url'].to_s.split('/').last}", "wb") do |file|
-			puts " downloading " + s.at('enclosure')['url'].to_s.split('/').last
-				file.write open("#{s.at('enclosure')['url']}").read
-			end
+  next if File.exist? s.at('enclosure')['url'].to_s.split('/').last
+  File.open("#{s.at('enclosure')['url'].to_s.split('/').last}", "wb") do |file|
+    puts " downloading " + s.at('enclosure')['url'].to_s.split('/').last
+    file.write open("#{s.at('enclosure')['url']}").read
+  end
 end
