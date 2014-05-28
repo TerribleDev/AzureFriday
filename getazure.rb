1b5cc6e49b2e7bc6c3acf3016734e0e4db14a64c
Tommy Parnell <parnell.tommy@hotmail.com>
2014-05-28 03:54:24 -0400

    init commit

---
+++ getazure.rb
@@ -0,0 +1,14 @@
+require 'rubygems'
+require 'open-uri'
+require 'Nokogiri'
+
+xml = Nokogiri::XML(open('http://s.ch9.ms/Shows/Azure-Friday/feed/mp4high'))
+item = xml.xpath('//item')
+
+item.each do |s|
+
+File.open("#{s.at('enclosure')['url'].to_s.split('/').last}", "wb") do |file|
+			puts " downloading " + s.at('enclosure')['url'].to_s.split('/').last
+				file.write open("#{s.at('enclosure')['url']}").read
+			end
+end
