b43e2f65d81ecf4974b70e85bb7e8712d04301af
Darren Cauthon <darren@cauthon.com>
2014-05-28 07:40:52 -0500

    A little spacing can help.

--- getazure.rb
+++ getazure.rb
@@ -14,7 +14,9 @@ podcasts.each do |podcast|
   source      = podcast.at('enclosure')['url'].to_s
   name        = source.split('/').last
   local_file  = "downloads/#{name}"
+
   next if File.exist? local_file
+
   File.open(local_file, "wb") do |file|
     puts " downloading " + name
     file.write open(source).read
