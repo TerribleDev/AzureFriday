c618ee66efa8f212f8a8e0c27d3cffaac711ff01
Darren Cauthon <darren@cauthon.com>
2014-05-28 07:37:36 -0500

    More refactors.

--- getazure.rb
+++ getazure.rb
@@ -4,15 +4,15 @@ require 'nokogiri'
 require 'fileutils'
 
 FileUtils.mkdir_p 'downloads/'
-FileUtils.cd 'downloads/'
 xml = Nokogiri::XML(open('http://s.ch9.ms/Shows/Azure-Friday/feed/mp4high'))
 item = xml.xpath('//item')
 
 item.each do |s|
-  source = s.at('enclosure')['url'].to_s
-  name   = source.split('/').last
-  next if File.exist? name
-  File.open(name, "wb") do |file|
+  source      = s.at('enclosure')['url'].to_s
+  name        = source.split('/').last
+  local_file  = "downloads/#{name}"
+  next if File.exist? local_file
+  File.open(local_file, "wb") do |file|
     puts " downloading " + name
     file.write open(source).read
   end
